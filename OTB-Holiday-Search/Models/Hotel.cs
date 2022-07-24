0357c9081c4886a232cfe5f1a75ca2622cd0621d
Sam <snippetsbysam@outlook.com>
2022-07-24 02:27:35 +0100

    add hotel model

--- OTB-Holiday-Search/Models/Hotel.cs
+++ OTB-Holiday-Search/Models/Hotel.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace OTB_Holiday_Search.Models
 {
-    internal class Hotel
+    public class Hotel
     {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("arrival_date")]
+        public DateTime ArrivalDate { get; set; }
+        [JsonPropertyName("price_per_night")]
+        public decimal PricePerNight { get; set; }
+        [JsonPropertyName("local_airports")]
+        public string[] LocalAirports { get; set; }
+        [JsonPropertyName("nights")]
+        public int Nights { get; set; }
     }
 }
