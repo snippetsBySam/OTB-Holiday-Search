74dbf660a14c39bc6c2193314808c21d37c80c99
Sam <snippetsbysam@outlook.com>
2022-07-24 02:21:07 +0100

    add flight model

--- OTB-Holiday-Search/Models/Flight.cs
+++ OTB-Holiday-Search/Models/Flight.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace OTB_Holiday_Search.Models
 {
-    internal class Flight
+    public class Flight
     {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("airline")]
+        public string Airline { get; set; }
+        [JsonPropertyName("from")]
+        public string From { get; set; }
+        [JsonPropertyName("to")]
+        public string To { get; set; }
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+        [JsonPropertyName("departure_date")]
+        public DateTime DepartureDate { get; set; }
     }
 }
