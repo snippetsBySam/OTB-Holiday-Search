a0b4e213f7f1f44f6e1ac5a8175a8fdd510e8c05
Sam <snippetsbysam@outlook.com>
2022-07-25 00:22:18 +0100

    add holiday query class

--- OTB-Holiday-Search/Models/Query/HolidayQuery.cs
+++ OTB-Holiday-Search/Models/Query/HolidayQuery.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace OTB_Holiday_Search.Models.Query
 {
-    internal class HolidayQuery
+    public class HolidayQuery
     {
+        internal string[] DepartingFrom;
+        internal string TravellingTo;
+        internal DateTime DepartureDate;
+        internal int Duration;
+        public HolidayQuery(string[] departingFrom, string travellingTo, DateTime departureDate, int duration)
+        {
+            DepartingFrom = departingFrom;
+            TravellingTo = travellingTo;
+            DepartureDate = departureDate;
+            Duration = duration;
+        }
     }
 }
