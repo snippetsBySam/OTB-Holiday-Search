bdd4fde8670d623f51740592ea8162bb81387d9b
Sam <snippetsbysam@outlook.com>
2022-07-24 22:08:15 +0100

    Add first red test

--- OTB-Holiday-Search/Services/FlightService.cs
+++ OTB-Holiday-Search/Services/FlightService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OTB_Holiday_Search.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace OTB_Holiday_Search.Services
 {
-    internal class FlightService
+    public class FlightService
     {
+        public List<Flight> GetFlightsFromJson()
+        {            
+            return new List<Flight>();
+        }
     }
 }
