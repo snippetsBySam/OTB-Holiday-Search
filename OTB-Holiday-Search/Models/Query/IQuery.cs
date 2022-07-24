2bcccef79c16928c94a963143ba1798480ffd846
Sam <snippetsbysam@outlook.com>
2022-07-24 02:17:33 +0100

    update folder structure

---
+++ OTB-Holiday-Search/Models/Query/IQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTB_Holiday_Search.Models.Query
+{
+    internal interface IQuery
+    {
+    }
+}
---
+++ OTB-Holiday-Search/Models/Search/ISearch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTB_Holiday_Search.Models.Search
+{
+    internal interface ISearch
+    {
+    }
+}
