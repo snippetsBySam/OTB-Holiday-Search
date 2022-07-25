34a1a7f9d3a76f22435549297a380d219eb6421d
Sam <snippetsbysam@outlook.com>
2022-07-25 03:16:07 +0100

    add more tests for Holiday results

--- OTB-Holiday-Search/Models/Search/HolidaySearch.cs
+++ OTB-Holiday-Search/Models/Search/HolidaySearch.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OTB_Holiday_Search.Models.Query;
+using OTB_Holiday_Search.Models.Result;
+using OTB_Holiday_Search.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,41 @@ using System.Threading.Tasks;
 
 namespace OTB_Holiday_Search.Models.Search
 {
-    internal class HolidaySearch
+    public class HolidaySearch
     {
+        private List<HolidayResult> _holidayResult = new List<HolidayResult>();
+        private List<Flight> _matchingFlights = new List<Flight>();
+        private List<Hotel> _matchingHotels = new List<Hotel>();
+
+        public HolidaySearch(HolidayQuery query)
+        {
+
+            this._matchingFlights = new FlightService().GetFlightsFromJson().Where(flight =>
+                query.DepartingFrom.Any(location => location == flight.From) &&
+                query.TravellingTo == flight.To &&
+                query.DepartureDate == flight.DepartureDate).ToList();
+            this._matchingHotels = new HotelService().GetHotelsFromJson().Where(hotel =>
+                hotel.LocalAirports.Any(airports => airports == query.TravellingTo) &&
+                query.DepartureDate == hotel.ArrivalDate).ToList();
+            BuildResult();
+        }
+
+        public List<HolidayResult> GetResultsDefault()
+        {
+            return this._holidayResult;
+        }
+        private void BuildResult()
+        {
+            if (_matchingFlights.Count > 0 && _matchingHotels.Count > 0)
+            {
+                _matchingFlights.ForEach(flight =>
+                    _matchingHotels.ForEach(hotel =>
+                        _holidayResult.Add(new HolidayResult(flight, hotel))));
+            }
+            else
+            {
+                throw new Exception("Not enough Results");
+            }
+        }
     }
 }
